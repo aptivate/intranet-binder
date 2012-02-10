fec38a5d164c63c1d5919655894bca2a4a8980da
Chris Wilson <chris+github@qwirx.com>
2012-02-10 16:24:32 +0000

    Cleanup comments, add newline at end of file.

--- tests.py
+++ tests.py
@@ -1,10 +1,3 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.contrib.auth.models import User
 from django.conf import settings
 from django.core.urlresolvers import reverse
