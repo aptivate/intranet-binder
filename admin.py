3aa9e969fc5c5a02ed9d35f3c6c17830bbb5e019
Chris Wilson <chris+github@qwirx.com>
2012-04-03 16:16:45 +0100

    Fix blank fields on read-only admin forms.
    AdminReadonlyField.contents() does not actually take a widget_attrs
    parameter, and template rendering swallows the resulting exception.

--- admin.py
+++ admin.py
@@ -596,7 +596,7 @@ class CustomAdminReadOnlyField(AdminReadonlyField):
         if hasattr(form[field].field.widget, 'has_readonly_view'):
             return form[field].as_widget(attrs=widget_attrs)
         else:
-            return super(CustomAdminReadOnlyField, self).contents(widget_attrs)
+            return super(CustomAdminReadOnlyField, self).contents()
         
     # patch for https://code.djangoproject.com/ticket/16433
     def help_text_for_field(self, name, model):
