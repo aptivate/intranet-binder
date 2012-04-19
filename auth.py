6aefa37d083aea0cf7436bf2adfe50156f2c2f8b
Chris Wilson <chris+github@qwirx.com>
2012-04-19 11:07:34 +0300

    Add Management command to import users from Active Directory.

--- auth.py
+++ auth.py
@@ -40,6 +40,10 @@ class ActiveDirectoryBackend:
     """
     Copied from http://djangosnippets.org/snippets/501/
     """
+    
+    def __init__(self):
+        from django.contrib.auth.models import Group
+        self.default_group = Group.objects.get(name="User")
 
     def get_connection(self, username, password):
         l = ldap.initialize(settings.AD_LDAP_URL)
@@ -50,34 +54,50 @@ class ActiveDirectoryBackend:
         return l
     
     def authenticate(self,username=None,password=None):
+        # import pdb; pdb.set_trace()
+        
         if not self.is_valid(username,password):
             return None
+        
+        l = self.get_connection(username, password)
+        result = l.search_ext_s(settings.AD_SEARCH_DN, ldap.SCOPE_SUBTREE, 
+            "sAMAccountName=%s" % username,
+            settings.AD_SEARCH_FIELDS)[0]
+        l.unbind_s()
+        
+        user = self.create_or_update_user_from_result(dn=result[0], 
+            attrs=result[1],username=username, password=password)
+        
+        return user
+    
+    def create_or_update_user_from_result(self, dn, attrs, username,
+        password=None):
+
         try:
             user = IntranetUser.objects.get(username=username)
         except User.DoesNotExist:
-            l = self.get_connection(username, password)
-            result = l.search_ext_s(settings.AD_SEARCH_DN, ldap.SCOPE_SUBTREE, 
-                "sAMAccountName=%s" % username,
-                settings.AD_SEARCH_FIELDS)[0][1]
-            l.unbind_s()
+            user = IntranetUser()
+        
+        try:
+            user.username  = attrs['sAMAccountName'][0]
+            user.full_name = attrs['displayName'][0]
+            user.email     = attrs['mail'][0]
+            if 'mobile' in attrs:
+                user.cell_phone = attrs['mobile'][0]
+            if 'title' in attrs:
+                user.job_title = attrs['title'][0]
+        except KeyError as e:
+            raise KeyError("Required attribute %s missing from user %s: %s" %
+                (e, dn, attrs))
+        
+        user.is_staff = False
+        user.is_superuser = False
+        user.set_password(password)
+        user.save()
 
-            # givenName == First Name
-            if result.has_key('displayName'):
-                full_name = result['displayName'][0]
-            else:
-                full_name = None
-
-            # mail == Email Address
-            if result.has_key('mail'):
-                email = result['mail'][0]
-            else:
-                email = None
-
-            user = IntranetUser(username=username,full_name=full_name,email=email)
-            user.is_staff = False
-            user.is_superuser = False
-            user.set_password(password)
-            user.save()
+        if not user.groups.count():
+            user.groups.add(self.default_group)
+         
         return user
 
     def get_user(self,user_id):
---
+++ management/commands/import_ad_users.py
@@ -0,0 +1,77 @@
+"""
+Management utility to create superusers.
+"""
+
+import getpass
+import re
+import sys
+from optparse import make_option
+from django.core import exceptions
+from django.core.management.base import BaseCommand, CommandError
+from django.utils.translation import ugettext as _
+
+from binder.auth import ActiveDirectoryBackend
+
+class Command(BaseCommand):
+    """
+    option_list = BaseCommand.option_list + (
+        make_option('--username', dest='username', default=None,
+            help='Specifies the username for the superuser.'),
+        make_option('--email', dest='email', default=None,
+            help='Specifies the email address for the superuser.'),
+        make_option('--noinput', action='store_false', dest='interactive', default=True,
+            help=('Tells Django to NOT prompt the user for input of any kind. '
+                  'You must use --username and --email with --noinput, and '
+                  'superusers created with --noinput will not be able to log '
+                  'in until they\'re given a valid password.')),
+    )
+    """
+    
+    help = ('Used to import all users from the Active Directory server ' +
+        'configured in local_settings.py')
+
+    def handle(self, *args, **options):
+        """
+        username = options.get('username', None)
+        email = options.get('email', None)
+        interactive = options.get('interactive')
+        verbosity = int(options.get('verbosity', 1))
+
+        # Do quick and dirty validation if --noinput
+        if not interactive:
+            if not username or not email:
+                raise CommandError("You must use --username and --email with --noinput.")
+            if not RE_VALID_USERNAME.match(username):
+                raise CommandError("Invalid username. Use only letters, digits, and underscores")
+            try:
+                is_valid_email(email)
+            except exceptions.ValidationError:
+                raise CommandError("Invalid email address.")
+        """
+        
+        from django.conf import settings
+        login = raw_input("Your user name on %s: " % settings.AD_NT4_DOMAIN)
+        password = getpass.getpass("Password for %s on %s: " %
+            (login, settings.AD_NT4_DOMAIN))
+        
+        auth = ActiveDirectoryBackend()
+        import ldap
+        
+        try:
+            l = auth.get_connection(login, password)
+        except ldap.LDAPError as e:
+            raise Exception("Login failed: %s" % e)
+        
+        results = l.search_ext_s(settings.AD_SEARCH_DN, ldap.SCOPE_SUBTREE, 
+            "objectClass=user", settings.AD_SEARCH_FIELDS)
+        l.unbind_s()
+        
+        # import pdb; pdb.set_trace()
+        
+        for result in results:
+            try:
+                user = auth.create_or_update_user_from_result(dn=result[0],
+                    attrs=result[1], username=result[1]['sAMAccountName'][0])
+                print("Imported user: %s" % user.username)
+            except KeyError as e:
+                print("Failed to import user %s: %s" % (result[0], e))
