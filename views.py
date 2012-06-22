f9912bf8f3fdbf36d017de651ee3df8de6ec0ed8
Chris Wilson <chris-github@aptivate.org>
2012-06-22 11:36:44 +0100

    Show the configured user model on user profile forms.

--- views.py
+++ views.py
@@ -4,9 +4,10 @@ from django import forms, http
 from django.core.urlresolvers import reverse
 from django.views.generic.base import TemplateView
 
-from models import IntranetUser
 from password import PasswordChangeMixin
 from widgets import AdminImageWidgetWithThumbnail
+
+import configurable
 
 class FrontPageView(TemplateView):
     template_name = 'front_page.dhtml'
@@ -15,14 +16,14 @@ class UserProfileForm(PasswordChangeMixin, forms.ModelForm):
     required_css_class = 'required'
 
     class Meta:
-        model = IntranetUser
+        model = configurable.UserModel
         exclude = ('is_staff', 'is_active', 'is_superuser', 'password',
             'groups', 'user_permissions', 'first_name', 'last_name',
             'last_login', 'date_joined', 'date_left')
 
     password1 = forms.CharField(required=False, label="New password")
     password2 = forms.CharField(required=False, label="Confirm new password")
-    notes = IntranetUser._meta.get_field('notes').formfield(
+    notes = configurable.UserModel._meta.get_field('notes').formfield(
         help_text="""e.g. educational background,  professional experience,
         present job, personal interests, languages spoken etc.""")
     photo = forms.ImageField(required=False, widget=AdminImageWidgetWithThumbnail)
