1de18a5a351855525c108ef798eea8113f8b22bf
Chris Wilson <chris+github@qwirx.com>
2013-01-09 15:34:27 +0000

    Add IpAddressRangeField and tests.
    Remove tests for things that don't belong in this shared module.

--- modelfields.py
+++ modelfields.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 # from http://stackoverflow.com/questions/2350681/django-lowercasecharfield
 class ModifyingFieldDescriptor(object):
@@ -95,8 +96,135 @@ class ManyToManyField(related.ManyToManyField):
         # ReverseManyRelatedObjectsDescriptor
         setattr(cls, self.name, PatchedReverseManyRelatedObjectsDescriptor(self))
 
+class ManyToManyWithBlankForAll(ManyToManyField):
+    """
+    A version of ManyToManyField where a blank value is interpreted as
+    meaning "all" instead of "none". This is mainly an exercise/example
+    in customising the text displayed for the blank choice at the top
+    of the list, so that it works with checkbox sets, etc.
+    """
+     
+    def __init__(self, to, blank_choice=None, **kwargs):
+        super(ManyToManyWithBlankForAll, self).__init__(to,
+            blank=True, **kwargs)
+        if blank_choice is None:
+            blank_choice = "All %s" % to._meta.verbose_name_plural
+        self.blank_choice = blank_choice
+
+    def get_choices(self, include_blank=True):
+        import pdb; pdb.set_trace()
+        return super(ManyToManyWithBlankForAll, self).get_choices(
+            include_blank=True, blank_choice=(('', self.blank_choice),))
+            
+    def _get_choices(self):
+        import pdb; pdb.set_trace()
+        """
+        from itertools import chain
+        return chain((('', self.blank_choice),),
+            super(ManyToManyWithBlankForAll, self).get_choices())
+        """
+        return super(ManyToManyWithBlankForAll, self)._get_choices()
+
+    def _set_choices(self, choices):
+        import pdb; pdb.set_trace()
+        """
+        from itertools import chain
+        return chain((('', self.blank_choice),),
+            super(ManyToManyWithBlankForAll, self).get_choices())
+        """
+        return super(ManyToManyWithBlankForAll, self)._set_choices(choices)
+
+    choices = property(_get_choices, _set_choices)
+
+import re
+IP_ADDRESS_BYTE_REGEX = r'(0|[1-9][0-9]{0,2})'
+IP_ADDRESS_REGEX = (
+    '(?x)^' +
+    IP_ADDRESS_BYTE_REGEX + r'\.' +
+    IP_ADDRESS_BYTE_REGEX + r'\.' +
+    IP_ADDRESS_BYTE_REGEX + r'\.' +
+    IP_ADDRESS_BYTE_REGEX + 
+    '(?:/' + IP_ADDRESS_BYTE_REGEX + ')?$')
+    
+class IpAddressRangeValidator(object):
+    def __init__(self, message="Invalid IP address range", code="invalid",
+        regex=IP_ADDRESS_REGEX):
+        
+        self.message = message
+        self.code = code
+
+        # Compile the regex if it was not passed pre-compiled.
+        self.regex_string = regex
+        from django.utils import six
+        if isinstance(regex, six.string_types):
+            regex = re.compile(regex)
+        self.regex = regex
+
+    def __call__(self, value):
+        """
+        Validates that the input matches the regular expression.
+        """
+        from django.utils.encoding import force_text
+        from django.core.exceptions import ValidationError
+        
+        matches = self.regex.match(force_text(value))
+        if matches is None:
+            raise ValidationError(("%s: does not match the pattern: " +
+                "a.b.c.d or a.b.c.d/e") % self.message, code=self.code)
+        
+        ip_bits = long(0)
+        
+        for i in range(4):
+            octet = int(matches.group(i + 1))
+            if octet >= 0 and octet <= 255:
+                ip_bits <<= 8
+                ip_bits |= octet
+            else:
+                raise ValidationError(("%s: address byte %d (%s) " +
+                    "must be between 0 and 255") %
+                    (self.message, i, octet), code=self.code)
+        
+        if matches.group(5) is None:
+            # it's allowed to omit the mask length completely
+            mask_len = 32
+        else:
+            mask_len = int(matches.group(5))
+
+        if mask_len < 0 or mask_len > 32:
+            raise ValidationError(("%s: network mask length %s " +
+                "should be between 0 and 32") %
+                (self.message, matches.group(5)), code=self.code)
+        
+        mask = (long(1) << (32 - mask_len)) - 1
+        masked_value = (ip_bits & mask)
+        if masked_value != 0:
+            raise ValidationError(("%s: according to the network mask %d, " +
+                "the last %d bits should be 0, but they are %d instead. " +
+                "Please check the address and mask") %
+                (self.message, mask_len, 32 - mask_len, masked_value),
+                code=self.code)
+
+from django.db.models.fields import Field
+class IpAddressRangeField(Field):
+    description = _("IPv4 address and network mask (CIDR)")
+
+    def __init__(self, validators=[], *args, **kwargs):
+        kwargs['max_length'] = 15
+        
+        validators.append(IpAddressRangeValidator())
+        super(IpAddressRangeField, self).__init__(validators=validators,
+            *args, **kwargs)
+    
+    """
+    def validate(self, value, model_instance):
+        import pdb; pdb.set_trace()
+        super(IpAddressRangeField, self).validate(value, model_instance)
+    """
+
 from django.conf import settings
 if 'south' in settings.INSTALLED_APPS:
     from south.modelsinspector import add_introspection_rules
     import re
-    add_introspection_rules([], ["^%s\.ManyToManyField$" % re.escape(ManyToManyField.__module__)])
+    module_regexp = re.escape(ManyToManyField.__module__)
+    add_introspection_rules([], ["^%s\.ManyToManyField$" % module_regexp])
+    add_introspection_rules([], ["^%s\.IpAddressRangeField" % module_regexp])
--- tests.py
+++ tests.py
@@ -1,24 +1,16 @@
-import django.db.models
-
-import templatetags.menu as menu_tag
-
-from django.contrib.auth.models import User, Group
+from django.contrib.auth import get_user_model
 from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.template import Context
+UserModel = get_user_model()
 
-from models import IntranetUser
+from django_dynamic_fixture import G
+
 from test_utils import AptivateEnhancedTestCase
-
 from views import FrontPageView
 
 class BinderTest(AptivateEnhancedTestCase):
-    fixtures = ['test_programs', 'test_permissions', 'test_users']
-
-    def setUp(self):
-        AptivateEnhancedTestCase.setUp(self)
-        self.john = IntranetUser.objects.get(username='john')
-        self.ringo = IntranetUser.objects.get(username='ringo')
+    fixtures = ['test_permissions', 'binder_test_users']
 
     def test_front_page_without_login(self):
         response = self.client.get('/')
@@ -40,6 +32,8 @@ class BinderTest(AptivateEnhancedTestCase):
             "Wrong main menu for unauthenticated users")
 
     def test_menu_tag_with_named_route(self):
+        import templatetags.menu as menu_tag
+        
         context = Context({'global':{'path':'/'}})
         self.assertEqual('<td class="selected"><a href="/">Home</a></td>',
             menu_tag.menu_item(context, 'td', 'front_page', 'Home'))
@@ -50,309 +44,10 @@ class BinderTest(AptivateEnhancedTestCase):
 
     def login(self, user=None):
         if user is None:
-            user = self.ringo
+            user = G(UserModel, password=self.test_password_encrypted)
         super(BinderTest, self).login(user)
 
-    def assert_logged_in_status_field(self, user, expected_value):
-        response = self.client.get(reverse('admin:binder_intranetuser_change',
-            args=[user.id]))
-
-        self.assertEqual(str(expected_value),
-            self.extract_admin_form_field(response,  'is_logged_in').contents())
-
-        response = self.client.get(reverse('admin:binder_intranetuser_readonly',
-            args=[user.id]))
-
-        self.assertEqual(expected_value, 
-            self.extract_admin_form(response).form['is_logged_in'].readonly())
-                
-    def test_logged_in_status_shown_in_admin_form(self):
-        self.login()
-        self.assertTrue(self.current_user.is_logged_in())
-        self.assert_logged_in_status_field(self.current_user, True)
-
-        self.assertNotEqual(self.john, self.current_user)
-        self.assert_logged_in_status_field(self.john, False)
-        
-        previous_user = self.current_user
-        response = self.client.get(reverse('logout'))
-        self.assertEqual(200, response.status_code)
-        self.assertEqual('Logged out', response.context['title'])
-        self.assertFalse(previous_user.is_logged_in())
-
    
-    def test_profile_picture_shown_in_user_admin_and_profile_forms(self):
-        self.login()
-        response = self.client.get(reverse('admin:binder_intranetuser_readonly',
-            args=[self.john.id]))
-        field = self.extract_admin_form_field(response, 'photo')
-        
-        from widgets import AdminImageWidgetWithThumbnail
-        widget = field.form.fields['photo'].widget
-        self.assertIsInstance(widget, AdminImageWidgetWithThumbnail)
-
-        response = self.client.get(reverse('admin:binder_intranetuser_change',
-            args=[self.john.id]))
-        field = self.extract_admin_form_field(response, 'photo')
-        widget = field.field.field.widget
-        self.assertIsInstance(widget, AdminImageWidgetWithThumbnail)
-        
-        response = self.client.get(reverse('user_profile'))
-        form = self.assertInDict('profile_form', response.context)
-        field = self.assertInDict('photo', form.fields)
-        self.assertIsInstance(field.widget, AdminImageWidgetWithThumbnail)
-        
-    def test_notes_field_for_user(self):
-        self.assertIsInstance(IntranetUser._meta.get_field('notes'),
-            django.db.models.TextField)
-
-    def test_ordinary_user_cannot_change_self_to_superuser(self):
-        self.login(self.john)
-        response = self.client.get(reverse('user_profile'))
-        self.assertIn('profile_form', response.context, "Where's my form? " +
-            "Am I really logged in?\n" + response.content)
-
-        form = self.assertInDict('profile_form', response.context)
-        data = self.update_form_values(form)
-        data['full_name'] = "Wheee"
-        data['is_superuser'] = True
-        
-        # import pdb; pdb.set_trace()
-        
-        response = self.client.post(reverse('user_profile'), data=data,
-            follow=True)
-
-        if ('profile_form' in response.context):
-            # should not happen, implies a form validation error?
-            form = response.context['profile_form']
-            self.assertItemsEqual([], form.errors,
-                "form should not have errors")
-        
-        self.assertTemplateUsed(response, FrontPageView.template_name,
-            'profile page should redirect to front page on success: ' +
-            '%s' % response)
-
-        new_john = IntranetUser.objects.get(id=self.john.id)
-        self.assertFalse(new_john.is_superuser)
-        self.assertEqual("Wheee", new_john.full_name)
-    
-    def test_adding_user_to_administrators_group_sets_superuser_flag(self):
-        from models import IntranetGroup 
-        manager = IntranetGroup.objects.get(name="Manager")
-        user = IntranetGroup.objects.get(name="User")
-        self.assertTrue(manager.administrators, """This test will not work 
-            unless the Manager group's administrators flag is set""")
-        
-        self.assertFalse(self.john.is_superuser)
-        self.assertNotIn(manager, self.john.groups.all(),
-            "This test will not work if john is in the Manager group")
-        
-        self.john.groups = [manager]
-        self.john.save()
-        self.assertTrue(self.john.is_superuser)
-
-        self.john.groups = [manager]
-        self.john.save()
-        self.assertTrue(self.john.is_superuser)
-
-        self.john.groups = [user]
-        self.john.save()
-        self.assertFalse(self.john.is_superuser)
-        
-    def test_admin_form_should_stop_user_demoting_themselves(self):
-        self.login()
-        
-        from models import IntranetGroup 
-        manager = IntranetGroup.objects.get(name="Manager")
-        self.assertTrue(manager.administrators, """This test will not work 
-            unless the Manager group's administrators flag is set""")
-        self.assertTrue(self.current_user.is_superuser)
-        self.assertIn(manager.group, self.current_user.groups.all())
-
-        url = reverse('admin:binder_intranetuser_change',
-            args=[self.current_user.id])
-        response = self.client.get(url)
-
-        # POST without changing anything should be fine
-        form = self.assertInDict('adminform', response.context).form
-        new_values = self.update_form_values(form)
-        response = self.client.post(url, new_values, follow=True)
-        self.assert_changelist_not_admin_form_with_errors(response)
-
-        # but changing the group should result in an error
-        user = IntranetGroup.objects.get(name="User")
-        new_values = self.update_form_values(form, groups=[user.pk])
-        response = self.client.post(url, new_values)
-        self.assert_admin_form_with_errors_not_changelist(response,
-            {'groups': ['You cannot demote yourself from the %s group' %
-                manager.name]})
-        
-        # shouldn't be allowed to do anything that removes our superuser flag
-        # remove us from manager group, but keep superuser flag.
-        # temporarily disable the signal listener so that it doesn't
-        # automatically demote us from superuser
-        from django.db.models.signals import m2m_changed
-        from django.dispatch import receiver
-        m2m_changed.disconnect(sender=User.groups.through,
-            receiver=IntranetUser.groups_changed, dispatch_uid="User_groups_changed")
-        self.current_user.groups = [user]
-        m2m_changed.connect(sender=User.groups.through,
-            receiver=IntranetUser.groups_changed, dispatch_uid="User_groups_changed")
-        
-        self.current_user = self.current_user.reload()
-        self.assertItemsEqual([user.group], self.current_user.groups.all())
-        self.assertTrue(self.current_user.is_superuser)
-        # now we're not removing ourselves from any groups, but saving
-        # would still demote us automatically from being a superuser.
-        response = self.client.post(url, new_values)
-        self.assert_admin_form_with_errors_not_changelist(response,
-            {'groups': ['You cannot demote yourself from being a superuser. ' +
-                'You must put yourself in one of the Administrators groups: ' +
-                '%s' % IntranetGroup.objects.filter(administrators=True)]})
-        
-        # we shouldn't be allowed to delete ourselves either
-        deleted = IntranetGroup.objects.get(name="Deleted")
-        user = IntranetGroup.objects.get(name="User")
-        new_values = self.update_form_values(form, groups=[manager.pk, deleted.pk])
-        # import pdb; pdb.set_trace()
-        response = self.client.post(url, new_values)
-        self.assert_admin_form_with_errors_not_changelist(response,
-            {'groups': ['You cannot place yourself in the %s group' %
-                deleted.name]})
-        
-    def test_admin_form_should_allow_user_to_promote_and_demote_others(self):
-        self.login()
-        
-        from models import IntranetGroup 
-        manager = IntranetGroup.objects.get(name="Manager")
-        self.assertTrue(manager.administrators, """This test will not work 
-            unless the Manager group's administrators flag is set""")
-
-        self.assertIn(manager.group, self.current_user.groups.all())
-        self.assertTrue(self.current_user.is_manager)
-        self.assertTrue(self.current_user.is_superuser)
-        
-        self.assertNotIn(manager.group, self.john.groups.all())
-        self.assertFalse(self.john.is_manager)
-        self.assertFalse(self.john.is_superuser)
-
-        url = reverse('admin:binder_intranetuser_change',
-            args=[self.john.id])
-        response = self.client.get(url)
-
-        form = self.assertInDict('adminform', response.context).form
-        new_values = self.update_form_values(form, groups=[manager.pk])
-        response = self.client.post(url, new_values, follow=True)
-        self.assert_changelist_not_admin_form_with_errors(response)
-        self.assertTrue(self.john.reload().is_superuser)
-
-        user = IntranetGroup.objects.get(name="User")
-        new_values = self.update_form_values(form, groups=[user.pk])
-        response = self.client.post(url, new_values, follow=True)
-        self.assert_changelist_not_admin_form_with_errors(response)
-        self.assertFalse(self.john.reload().is_superuser)
-
-        # import pdb; pdb.set_trace()
-        self.assertTrue(self.john.reload().is_active,
-            "test precondition failed")
-        deleted = IntranetGroup.objects.get(inactive=True)
-        new_values = self.update_form_values(form, groups=[deleted.pk])
-        response = self.client.post(url, new_values, follow=True)
-        self.assert_changelist_not_admin_form_with_errors(response)
-        self.assertFalse(self.john.reload().is_active)
-
-    def test_can_create_users(self):
-        u = IntranetUser(username="max")
-        u.save()
-
-        self.login()
-        
-        from models import IntranetGroup 
-        manager = IntranetGroup.objects.get(name="Manager")
-        self.assertTrue(manager.administrators, """This test will not work 
-            unless the Manager group's administrators flag is set""")
-
-        self.assertIn(manager.group, self.current_user.groups.all())
-        self.assertTrue(self.current_user.is_manager)
-        self.assertTrue(self.current_user.is_superuser)
-        
-        url = reverse('admin:binder_intranetuser_add')
-        response = self.client.get(url)
-
-        form = self.assertInDict('adminform', response.context).form
-        # import pdb; pdb.set_trace()
-        self.assertNotIn('is_active', form.fields)
-        self.assertNotIn('is_staff', form.fields)
-        self.assertNotIn('is_superuser', form.fields)
-        
-        values = dict(username="stevie", groups=[manager.pk])
-        # enter some random value for all required fields
-        for field in form:
-            if field.field.required and field.name not in values:
-                # import pdb; pdb.set_trace()
-                from django.forms.fields import ChoiceField
-                
-                db_field = form._meta.model._meta.get_field(field.name)
-                from django.db.models.fields import DateTimeField
-                
-                if isinstance(field.field, ChoiceField): 
-                    values[field.name] = field.field.choices[1][0]
-                elif isinstance(db_field, DateTimeField):
-                    from datetime import datetime
-                    values[field.name] = datetime.now()
-                else:
-                    values[field.name] = "blarg"
-        
-        params = self.update_form_values(form, **values)
-        response = self.client.post(url, params, follow=True)
-        self.assert_changelist_not_admin_form_with_errors(response)
-        stevie = IntranetUser.objects.get(username="stevie")
-        self.assertTrue(stevie.is_active)
-        self.assertTrue(stevie.is_staff)
-        self.assertTrue(stevie.is_superuser)
-    
-    def test_create_user_with_photo_and_missing_required_fields(self):
-        self.login()
-        url = reverse('admin:binder_intranetuser_add')
-        response = self.client.get(url)
-
-        form = self.assertInDict('adminform', response.context).form
-        # import pdb; pdb.set_trace()
-        
-        import os
-        f = open(os.path.join(os.path.dirname(__file__), 'fixtures',
-            'transparent.gif'))
-        # setattr(f, 'name', 'transparent.gif')
-        
-        params = self.update_form_values(form, photo=f)
-        response = self.client.post(url, params, follow=True)
-        self.assert_admin_form_with_errors_not_changelist(response)
-    
-    def test_menu_contains_correct_user_model(self):
-        """
-        Even if the administrator has replaced the main menu with a custom
-        one in their application, the default main menu (from the binder app)
-        should still contain a link to the user manager for whatever the
-        configured user model is.
-        """
-        
-        self.login()
-        self.assertTrue(self.current_user.is_manager)
-        
-        from binder.main_menu import MainMenu
-        menu = MainMenu(self.fake_login_request)
-        
-        from configurable import UserModel
-        user_changelist = ('admin:%s_%s_changelist' %
-            (UserModel._meta.app_label, UserModel._meta.module_name))
-        
-        user_menu_item = [i for i in menu.generators 
-            if i.url_name == user_changelist][0]
-        from django.utils.text import capfirst
-        self.assertEqual(capfirst(UserModel._meta.verbose_name_plural), 
-            user_menu_item.title,
-            "Wrong title in menu item %s" % (user_menu_item,))
-
     def test_TemplatedModelChoiceField_renders_template_correctly(self):
         from configurable import UserModel
         from admin import (TemplatedModelChoiceField,
@@ -416,3 +111,38 @@ class BinderTest(AptivateEnhancedTestCase):
             if_empty_list(["a"], "nada"))
         self.assertEquals(["a", "b"],
             if_empty_list(["a", "b"], "nada"))
+
+    def test_ip_address_range_field_validator(self):
+        from intranet_binder.modelfields import IpAddressRangeField
+        
+        def assert_valid_range(range_text):
+            IpAddressRangeField().run_validators(range_text)
+        
+        assert_valid_range("1.2.3.4")
+        assert_valid_range("1.2.3.0/24")
+        assert_valid_range("0.0.0.0")
+        assert_valid_range("0.0.0.0/0")
+        assert_valid_range("0.0.0.0/32")
+
+        def assert_invalid_range(invalid_range_text, message):
+            from django.core.exceptions import ValidationError
+            with self.assertRaises(ValidationError) as e:
+                IpAddressRangeField().run_validators(invalid_range_text)
+    
+        assert_invalid_range("1.2.3.4/", "missing network mask after slash")
+        assert_invalid_range("0.0.0.0/-1", "negative network mask")
+        assert_invalid_range("0.0.0.0/33", "excessive network mask")
+        assert_invalid_range("0.0.0.0/ 32", "space before mask")
+        assert_invalid_range("0.0.0.0 /32", "space before slash")
+        assert_invalid_range("0.0.0.", "missing number after dot")
+        assert_invalid_range("0.0.0", "missing octet")
+        assert_invalid_range("0.0.0.0.", "excessive dot")
+        assert_invalid_range("0.0.0.0.0", "excessive octets")
+        assert_invalid_range("0.0.0. 0", "space in address")
+        assert_invalid_range("0.0 0.0", "space instead of dot")
+        assert_invalid_range("0.00.0.0", "double zero")
+        assert_invalid_range("0.01.0.0", "leading zero")
+        assert_invalid_range("0.256.0.0", "excessive octet value")
+        assert_invalid_range("0.a.0.0", "letter instead of octet")
+        assert_invalid_range("1.2.3.4/24", "last octet is not 0, " +
+            "which conflicts with network mask")
