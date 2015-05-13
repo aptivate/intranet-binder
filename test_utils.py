3cb65ddc97f5a873e686241653b72ce62e2236c8
Hamish Downer <hamish@aptivate.org>
2015-05-13 16:54:23 +0100

    Fixes to work well with option groups

--- test_utils.py
+++ test_utils.py
@@ -393,13 +393,13 @@ class FormUtilsMixin(object):
                 # RadioSelect widget. In that case, we don't add anything
                 # to the POST data.
                 return {}
-            elif len(choices) == 0:
+            elif len(possible_values) == 0:
                 # it's possible to select no option in a drop-down list with
                 # no options!
                 return {}
             else:
                 # most browsers pre-select the first value
-                return {name: str(choices[0][0])}
+                return {name: str(possible_values[0])}
 
         elif isinstance(widget, django.contrib.admin.widgets.RelatedFieldWidgetWrapper):
             subwidget = widget.widget
