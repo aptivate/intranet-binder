6057db4280e16df07df4a10ab53bc178f0c6bdf4
Hamish Downer <hamish@aptivate.org>
2015-10-01 12:32:26 +0100

    rename url_replace -> url_get_params_replace

--- templatetags/url_get_params_replace.py
+++ templatetags/url_get_params_replace.py
@@ -4,7 +4,7 @@ register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
-def url_replace(context, field, value):
+def url_get_params_replace(context, field, value):
     """ The idea here is to take a GET dict and replace one value in the
     GET dict (or create it if it doesn't exist) and then return the encoded
     value.  Useful for pagination.
--- tests.py
+++ tests.py
@@ -120,14 +120,14 @@ class BinderTest(AptivateEnhancedTestCase):
             'request': factory.get('/', data=get_dict)
         }
 
-    def test_url_replace_templatetags_with_field_not_present(self):
-        from templatetags.url_replace import url_replace
-        path = url_replace(self.fake_context({'q': 'x'}), 'page', '3')
+    def test_url_get_params_replace_templatetags_with_field_not_present(self):
+        from templatetags.url_get_params_replace import url_get_params_replace
+        path = url_get_params_replace(self.fake_context({'q': 'x'}), 'page', '3')
         self.assertEquals('q=x&page=3', path)
 
-    def test_url_replace_templatetags_with_field_present(self):
-        from templatetags.url_replace import url_replace
-        path = url_replace(self.fake_context({'q': 'x', 'page': '1'}), 'page', '3')
+    def test_url_get_params_replace_templatetags_with_field_present(self):
+        from templatetags.url_get_params_replace import url_get_params_replace
+        path = url_get_params_replace(self.fake_context({'q': 'x', 'page': '1'}), 'page', '3')
         self.assertEquals('q=x&page=3', path)
 
     def test_ip_address_range_field_validator(self):
